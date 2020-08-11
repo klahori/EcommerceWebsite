17e5b3898c17fa0045639833079ce3f9304b7526
klahori <klaho028@uottawa.ca>
2020-08-11 11:26:57 -0400

    guests can now add items to cart

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -8,11 +8,31 @@ for (i = 0; i < updateBtns.length; i++) {
     console.log("USER:", user);
 
     if (user == "AnonymousUser") {
-      console.log("User is not authenticated");
+      addCookieItem(productId, action);
     } else {
       updateUserOrder(productId, action);
     }
   });
+}
+
+function addCookieItem(productId, action) {
+  console.log("User is not authenicated");
+  if (action == "add") {
+    if (cart[productId] == undefined) {
+      cart[productId] = { quantity: 1 };
+    } else {
+      cart[productId]["quantity"] += 1;
+    }
+  }
+  if (action == "remove") {
+    cart[productId]["quantity"] -= 1;
+    if (cart[productId]["quantity"] <= 0) {
+      console.log("item should be deleted");
+      delete cart[productId];
+    }
+  }
+  console.log("Cart:", cart);
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
 }
 
 function updateUserOrder(productId, action) {
